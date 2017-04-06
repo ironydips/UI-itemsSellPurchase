92aefdad3ffd65c064c196c94ba289e7d0b31baf
SupriyaSgn <supriyasingh.ggit@gmail.com>
2017-04-06 18:53:44 +0530

    modification in Checkout

--- app/salesPurchase/sales-purchase-checkout/sales-purchase-checkout.component.js
+++ app/salesPurchase/sales-purchase-checkout/sales-purchase-checkout.component.js
@@ -3,7 +3,30 @@
 
 function SalesPurchaseCheckoutController($state, checkoutService) {
 	var ctrl = this;
+	ctrl.collapse = false;
+	ctrl.secondCollapse = true;
+	ctrl.ThirdCollapse = true;
+	ctrl.getItems = checkoutService.getAddedItems();
 	console.log(checkoutService.getAddedItems())
+
+	ctrl.openGenrlInfo = function(){
+		ctrl.collapse = true;
+		ctrl.secondCollapse = false;
+		ctrl.ThirdCollapse = true;
+	};
+	ctrl.editSelectedItem = function(){
+		ctrl.collapse = false;
+		ctrl.secondCollapse = true;
+		ctrl.ThirdCollapse = true;
+	};
+	ctrl.editPayment = function(){
+		ctrl.collapse = true;
+		ctrl.secondCollapse = true;
+		ctrl.ThirdCollapse = false;
+	};
+	ctrl.deleteItem = function (item) {
+        ctrl.getItems.splice(ctrl.getItems.indexOf(item), 1);
+    }
 }
 
 angular.module('salesPurchaseCheckout')
--- app/salesPurchase/salesPurchase-sub-panel/salesPurchase-sub-panel.component.js
+++ app/salesPurchase/salesPurchase-sub-panel/salesPurchase-sub-panel.component.js
@@ -5,6 +5,7 @@ function SalesPurchaseSubPanelController($state, $timeout, checkoutService) {
     ctrl.selectedItem = {};
     ctrl.itemArr = [];
     ctrl.itemCount = 0;
+    
 
     ctrl.setBtnValue = function(value, index) {
 
@@ -14,32 +15,85 @@ function SalesPurchaseSubPanelController($state, $timeout, checkoutService) {
     ctrl.quickAdd = function(brand, price, qty) {
 
         ctrl.loader = true;
+        var totalPrice = parseInt(price) * parseInt(qty);
 
         var obj = {
             "imageUrl": "https://pizzaonline.dominos.co.in/themes/OLO_v2.1/images/deal/p-1.jpg",
             "brand": brand,
             "price": price,
             "qty": qty,
-            "size": ctrl.valueSet
+            "size": ctrl.valueSet,
+            "totalPrice": totalPrice
         }
-        console.log(obj)
         ctrl.selectedItem = {};
         ctrl.valueSet = "";
 
+        if (ctrl.itemArr.length < 0) {
 
-        // for (var i = 0; i <= ctrl.itemArr.length; i++) {
-    //     if (ctrl.itemArr.length == 0 || ctrl.itemArr[i].brand != obj.brand) {
-    //         ctrl.itemArr.push(obj);
-    //         console.log(ctrl.itemArr)
-    //     } else {
-    //         console.log("not in the list")
-    //     }
-    // }
+            ctrl.itemArr.push(obj);
+            console.log(ctrl.itemArr);
 
-        ctrl.itemArr.push(obj);
-        checkoutService.addItems(ctrl.itemArr);
+        } else {
 
+            var itemSet = ctrl.itemArr.filter(function(el) {
+                return (el.brand === brand);
 
+            });
+            if (itemSet.length == 0) {
+                ctrl.itemArr.push(obj);
+                checkoutService.addItems(ctrl.itemArr);
+                console.log(ctrl.itemArr);
+            } else {
+                //ctrl.getItems = checkoutService.getAddedItems();
+                // var itemSet = ctrl.itemArr.filter(function(el) {
+                //     return (el.brand === brand);
+
+                // });
+
+                angular.forEach(ctrl.itemArr, function(item, index) {
+                    if (item.brand === brand) {
+                    	if(item.size != obj.size){
+                    		ctrl.itemArr[index].size = obj.size;
+                    	}
+                    	if(item.qty != obj.qty){
+                    		ctrl.itemArr[index].qty = parseInt(ctrl.itemArr[index].qty) + parseInt(obj.qty);
+                    	}
+                    	if(item.price != obj.price){
+                    		ctrl.itemArr[index].price = parseInt(ctrl.itemArr[index].price) + parseInt(obj.price);
+                    	}
+                    	if(item.totalPrice != obj.totalPrice){
+                    		ctrl.itemArr[index].totalPrice = parseInt(ctrl.itemArr[index].totalPrice) + parseInt(obj.totalPrice);
+                    	}
+                        console.log(ctrl.itemArr);
+                        checkoutService.addItems(ctrl.itemArr);
+
+                    }
+                });
+                // angular.forEach(ctrl.getItems, function(item, index) {
+                //     if (item.brand === brand) {
+                //     	if(item.size != obj.size){
+                //     		ctrl.getItems[index].size = obj.size;
+                //     	}
+                //     	if(item.qty != obj.qty){
+                //     		ctrl.getItems[index].qty = obj.qty;
+                //     	}
+                //     	if(item.price != obj.price){
+                //     		ctrl.getItems[index].qty = obj.qty;
+                //     	}
+                //     	if(item.totalPrice != obj.totalPrice){
+                //     		ctrl.getItems[index].price = ctrl.getItems[index].price + obj.price;
+                //     	}
+                //         console.log(ctrl.getItems);
+                //         checkoutService.addItems(ctrl.getItems);
+                //         // ctrl.itemArr[index].qty = obj.qty;
+                //         // ctrl.itemArr[index].price = ctrl.itemArr[index].price + obj.price;
+                //        // ctrl.itemArr[index].size = obj.size;
+
+                //     }
+                // });
+                console.log(ctrl.itemArr);
+            }
+        }
 
         var countUp = function() {
             ctrl.itemCount = ctrl.itemArr.length;
@@ -50,18 +104,11 @@ function SalesPurchaseSubPanelController($state, $timeout, checkoutService) {
             //ctrl.loader = false;
         }
 
-        // if (ctrl.itemArr.indexOf(obj) == -1) {
-        // 	ctrl.itemArr.push(obj);
-        //    console.log(ctrl.itemArr)
-        //    var index = arr.indexOf(item);
-        // console.log(index)
-        // }
-
     }
 }
 
 angular.module('salesPurchaseSubPanel')
     .component('salesPurchaseSubPanel', {
         templateUrl: 'salesPurchase/salesPurchase-sub-panel/salesPurchase-sub-panel.template.html',
-        controller: ['$state', '$timeout','checkoutService', SalesPurchaseSubPanelController]
+        controller: ['$state', '$timeout', 'checkoutService', SalesPurchaseSubPanelController]
     });
